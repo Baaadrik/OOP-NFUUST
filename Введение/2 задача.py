8a2a26fba15d595c651ef7b0f01cf61c86dcbbb5
Badrik <denbadrik@mail.ru>
2024-03-16 16:47:56 +0300

    1-7

---
+++ Введение/2 задача.py
@@ -0,0 +1,20 @@
+class Button:
+    def __init__(self):
+        self.count = 0
+
+    def click(self):
+        self.count += 1
+
+    def click_count(self):
+        return self.count
+
+    def reset(self):
+        self.count = 0
+
+
+# Ваш код
+
+button = Button()
+button.click()
+print(button.click_count())
+
